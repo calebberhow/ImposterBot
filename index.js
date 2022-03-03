c301dee168c31a64937db9433a4038ddf6be4395
CressyCode <100820418+CressyCode@users.noreply.github.com>
2022-03-02 21:25:32 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+//AHAHAHAHHAHAHAHAA I AM A PORK CHOP
 // Load the bot's events and commands
 require('./util/eventLoader')(client);
 require('./util/commandLoader')(client);
