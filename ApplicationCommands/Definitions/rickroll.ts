c4e4263b805d3c78f1d93c8fa7181e49e7993ce3
Khazaari <62085209+calebberhow@users.noreply.github.com>
2023-12-28 23:07:22 -0800

    add rickroll start/stop commands.

--- ApplicationCommands/Definitions/rickroll.ts
+++ ApplicationCommands/Definitions/rickroll.ts
@@ -1,11 +1,35 @@
 import play from 'play-dl';
 import chalk from 'chalk';
 import ServiceClient from '../../ServiceClient.js';
-import { ChannelType, ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
-import { AudioPlayerState, AudioPlayerStatus, VoiceConnection, createAudioPlayer, createAudioResource, joinVoiceChannel } from '@discordjs/voice';
+import { ChannelType, ChatInputCommandInteraction, PermissionFlagsBits, SlashCommandBuilder } from 'discord.js';
+import { AudioPlayerStatus, VoiceConnection, createAudioPlayer, createAudioResource, joinVoiceChannel } from '@discordjs/voice';
 import ApplicationCommand from '../Infrastructure/ApplicationCommand.js';
 
 async function execute(client: ServiceClient, interaction: ChatInputCommandInteraction)
+{
+  switch (interaction.options.getSubcommand())
+  {
+    case 'start':
+      return start(client, interaction);
+    case 'stop':
+      return stop(client, interaction);
+    default:
+      return;
+  }
+}
+
+async function stop(client: ServiceClient, interaction: ChatInputCommandInteraction)
+{
+  if (client.Services.EventAggregator.HasListener(GetEvent(interaction.guildId)))
+  {
+    client.Services.EventAggregator.Invoke(GetEvent(interaction.guildId), interaction);
+    return;
+  }
+
+  await interaction.reply({ content: "Not currently playing... :/" });
+}
+
+async function start(client: ServiceClient, interaction: ChatInputCommandInteraction)
 {
   let channel = interaction.options.getChannel<ChannelType.GuildVoice>('voice_channel');
   let connection = joinVoiceChannel({ guildId: interaction.guildId, channelId: channel.id, adapterCreator: interaction.guild.voiceAdapterCreator });
@@ -17,21 +41,42 @@ async function execute(client: ServiceClient, interaction: ChatInputCommandInter
   let audio_player = createAudioPlayer();
   connection.subscribe(audio_player);
   audio_player.play(audio_resource);
-  audio_player.on(AudioPlayerStatus.Idle, destroyVC.bind(null, connection));
+
+  let id = client.Services.EventAggregator.Subscribe(GetEvent(interaction.guildId), async (event) =>
+  {
+    connection.destroy();
+    let newInteraction = event as ChatInputCommandInteraction;
+    await newInteraction.deferReply();
+    await newInteraction.deleteReply();
+    client.Services.EventAggregator.Unsubcribe(id);
+  });
+
+  audio_player.on(AudioPlayerStatus.Idle, () =>
+  {
+    connection.destroy();
+    client.Services.EventAggregator.Unsubcribe(id);
+  });
 }
 
-function destroyVC(connection: VoiceConnection): void
+function GetEvent(guild_id: string)
 {
-  connection.destroy();
+  return `rickroll_stop_${guild_id}`;
 }
 
 const builder = new SlashCommandBuilder()
   .setName('rickroll')
-  .setDescription('rickrolls the given audio channel')
-  .addChannelOption(opt => opt
-    .setName('voice_channel')
-    .setDescription('input the voice channel for imposterbot to join')
-    .addChannelTypes(ChannelType.GuildVoice)
-    .setRequired(true));
+  .setDescription('starts/stops rickroll.')
+  .addSubcommand(cmd => cmd
+    .setName('start')
+    .setDescription('starts rickroll on the given channel.')
+    .addChannelOption(opt => opt
+      .setName('voice_channel')
+      .setDescription('input the voice channel for imposterbot to join')
+      .addChannelTypes(ChannelType.GuildVoice)
+      .setRequired(true)))
+  .addSubcommand(cmd => cmd
+    .setName('stop')
+    .setDescription('stops ImposterBot from rickrolling.'))
+  .setDefaultMemberPermissions(PermissionFlagsBits.Speak | PermissionFlagsBits.Connect);
 
 export default new ApplicationCommand(builder, execute);
